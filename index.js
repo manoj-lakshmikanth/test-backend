e3ec467948b37ba51e639f1c16fbedca008f9dc3
manoj-lakshmikanth <manoj.lakshmikanth@gmail.com>
2023-04-21 21:40:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+let app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+const { MongoClient } = require('mongodb');
+const con = new MongoClient('mongodb://0.0.0.0:27017');
+let port = 8000;
+
+app.post('/add', async (req, resp) => {
+  try {
+    let data = req.body;
+    let connect = await con.connect();
+    let dbConnect = await connect.db('users');
+    let table = await dbConnect.collection('userDetails');
+    let inputData = await table.insertOne(data);
+    resp.status(200).json({ message: 'added', data: inputData });
+  } catch (error) {
+    resp.status(400).json({ message: 'error', error });
+  }
+});
+
+app.get('/get', async (req, resp) => {
+  try {
+    let connect = await con.connect();
+    let dbConnect = await connect.db('users');
+    let table = await dbConnect.collection('userDetails');
+    let getData = await table.find().toArray();
+    resp.status(200).json({ message: 'read', data: getData });
+  } catch (error) {
+    resp.status(400).json({ message: 'error', error });
+  }
+});
+
+app.listen(port, () => {
+  console.log('server is live');
+});
